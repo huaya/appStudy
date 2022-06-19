d02ce46c7fe60f2d3c70d38475bf63a8fbb981be
huaya55 <hu5624548@163.com>
2022-06-20 00:05:15 +0800

    commit

--- app/src/main/java/com/example/appstduy/QuizActivity.java
+++ app/src/main/java/com/example/appstduy/QuizActivity.java
@@ -2,6 +2,7 @@ package com.example.appstduy;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -10,6 +11,10 @@ import java.util.logging.LogManager;
 import java.util.logging.Logger;
 
 public class QuizActivity extends AppCompatActivity {
+
+    private static final String TAG = "QuizActivity";
+
+    private static final String KEY_INDEX = "index";
 
     private Button mTrueButton;
 
@@ -35,9 +40,13 @@ public class QuizActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate(Bundle) called");
         setContentView(R.layout.activity_quiz);
 
         mQuestionTextView = findViewById(R.id.question_text_view);
+        if(savedInstanceState != null){
+            mCurrentIndex = savedInstanceState.getInt(KEY_INDEX, 0);
+        }
         updateQuestion();
         mQuestionTextView.setOnClickListener(view -> {
             if(mCurrentIndex < mQuestionBank.length) {
@@ -54,7 +63,7 @@ public class QuizActivity extends AppCompatActivity {
 
         mFalseButton = findViewById(R.id.false_button);
         mFalseButton.setOnClickListener(view -> {
-            Logger.getLogger("QuizActivity").info("false button click!");
+            Log.i(TAG, "false button click!");
             checkAnswer(false);
         });
 
@@ -73,6 +82,43 @@ public class QuizActivity extends AppCompatActivity {
             }
             updateQuestion();
         });
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+        Log.d(TAG, "onSaveInstanceState(Bundle) called");
+        savedInstanceState.putInt(KEY_INDEX, mCurrentIndex);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart() called");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause() called");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop() called");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume() called");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy() called");
     }
 
     private void updateQuestion() {
